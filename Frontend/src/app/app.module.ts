815cc860fac6eecf1c0de93fc8bcd5620e21e68c
zEEmperon <dan4ik2204.danya@gmail.com>
2022-06-06 20:49:04 +0300

    basic property card

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProjectCardComponent } from './core/project/project-card/project-card.component';
-import { ProjectListComponent } from './core/project/project-list/project-list.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PropertyCardComponent } from './core/property/property-card/property-card.component';
@@ -12,8 +10,6 @@ import { PropertyListComponent } from './core/property/property-list/property-li
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectCardComponent,
-    ProjectListComponent,
     NavBarComponent,
     PropertyCardComponent,
     PropertyListComponent
--- Frontend/src/app/core/property/property-card/property-card.component.ts
+++ Frontend/src/app/core/property/property-card/property-card.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-property-card',
+  selector: 'property-card',
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
+
+  property: any = {
+    name: 'House12',
+    type: 'House',
+    price: 1200
+  }
 
   constructor() { }
 

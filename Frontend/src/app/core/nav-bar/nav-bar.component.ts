d53a56ec7d7201ce65f349cc2065de555b3942d5
zEEmperon <dan4ik2204.danya@gmail.com>
2022-06-12 19:00:26 +0300

    nav-bar selector renaming

--- Frontend/src/app/core/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })

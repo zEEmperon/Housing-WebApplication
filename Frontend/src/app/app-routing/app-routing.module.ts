e9622ea4501e9636e091e41bece8c283f92cae49
zEEmperon <dan4ik2204.danya@gmail.com>
2022-06-18 10:27:07 +0300

    add routes for new components

--- Frontend/src/app/app-routing/app-routing.module.ts
+++ Frontend/src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,16 @@ import {RouterModule, Routes} from "@angular/router";
 import {AddPropertyComponent} from "../core/property/add-property/add-property.component";
 import {PropertyListComponent} from "../core/property/property-list/property-list.component";
 import {PropertyDetailComponent} from "../core/property/property-detail/property-detail.component";
+import {LoginComponent} from "../core/user/login/login.component";
+import {SignupComponent} from "../core/user/signup/signup.component";
 
 const routes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
   {path: 'rent-property', component: PropertyListComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   //{path: '**', component: PageNotFoundComponent}
 ];
 
